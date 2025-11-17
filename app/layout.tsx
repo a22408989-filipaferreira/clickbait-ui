2966c44b9b866ea456bfeb7cd9ee17563a7a3850
a22408989-filipaferreira <a22408989@alunos.ulht.pt>
2025-11-17 18:53:44 +0000

    navbar update

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,27 +22,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   const data = new Date();
 
   return (
     <html lang="en">
-      <body className='flex flex-col justify-start gap-4 p-20 items-center min-h-screen'>
-        
+      <body className="flex flex-col justify-start gap-4 p-20 items-center min-h-screen">
         <header className="flex flex-col items-center">
           <h1>React & Next.js</h1>
           <nav className="flex gap-4">
             <Link href="/">Intro</Link>
             <Link href="/about">Sobre</Link>
+            <Link href="/characteristics">Características</Link>
+            <Link href="/technologies">Tecnologias</Link>
           </nav>
         </header>
-        
+
         <main className="bg-blue-200 p-5 rounded-2xl max-w-2xl min-h-[70vh]">
           {children}
         </main>
-        
-        <footer>DIW {data.getFullYear()}</footer>
 
+        <footer>DIW {data.getFullYear()}</footer>
       </body>
     </html>
   );
